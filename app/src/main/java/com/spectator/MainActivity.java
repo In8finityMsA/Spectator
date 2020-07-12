4485e85e0dbb888abc2c8972494145427beb1953
In8finity <wintry3@gmail.com>
2020-07-12 20:05:34 +0300

    Revert "Revert "Test commit""
    This reverts commit 943d8b9eb2441f923734cf099896ad9de2989265.

--- app/src/main/java/com/spectator/MainActivity.java
+++ app/src/main/java/com/spectator/MainActivity.java
@@ -40,6 +40,7 @@ public class MainActivity extends AppCompatActivity {
     private TextView lastHour;
     private String jsonPath = "log.json";
     private JSONObject jsonFile;
+    private WorkWithJson jsonIO;
     private TextView count;
     private TextView time;
     private ArrayList<Voter> voters = new ArrayList<>();
@@ -64,28 +65,24 @@ public class MainActivity extends AppCompatActivity {
         rowsList = new ArrayList<LinearLayout>();
         voters = new ArrayList<Voter>();
 
-        String[] files = fileList();
-        jsonFile = createJSONObject();
         inflater = (LayoutInflater) this.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+
         //Reading from file on startup
-        for (String fileName:files) {
-            Log.e("FileWriting", fileName);
-            if (fileName.equals(jsonPath)) {
-                try {
-                    parseJson(new JSONObject(readJSONFromFile(jsonPath)));
-                } catch (JSONException e) {
-                    e.printStackTrace();
-                }
-                Log.e("FileWriting", String.format(Locale.GERMAN, "%d", voters.size()));
-                for (int j = 0 ; j < voters.size(); j++) {
-                    jsonFile = addVoterToJSON(jsonFile, voters.get(j), "voters");
-                    rowsList.add(makeNewRow(voters.get(j)));
-                    scrollList.addView(rowsList.get(rowsList.size() - 1));
-                    counter++;
-                }
-                total.setText(String.format(Locale.GERMAN, "%d", counter));
-            }
+        jsonIO = new WorkWithJson(this.getFilesDir(), jsonPath);
+        try {
+            voters = jsonIO.firstInit();
+        } catch (JSONException e) {
+            e.printStackTrace();
+            voters = new ArrayList<Voter>();
         }
+        //And initializing interface from voters array
+        for (int j = 0 ; j < voters.size(); j++) {
+            rowsList.add(makeNewRow(voters.get(j)));
+            scrollList.addView(rowsList.get(rowsList.size() - 1));
+            counter++;
+        }
+
+        total.setText(String.format(Locale.GERMAN, "%d", counter));
 
         //Timer for checking votes those are one hour old
         /*ScheduledThreadPoolExecutor exec = new ScheduledThreadPoolExecutor(1);
@@ -108,11 +105,11 @@ public class MainActivity extends AppCompatActivity {
                 //Adding it to the list
                 voters.add(newVoter);
                 //Adding it to the json file Object
-                jsonFile = addVoterToJSON(jsonFile, newVoter, "voters");
+                //jsonIO.addVoterToJSON(newVoter, "voters");
                 //Writing Object to json file
-                writeJSONToFile(jsonFile, jsonPath);
-
-                //dELLETED OLD COMMENT
+                //jsonIO.writeToFile();
+                //Writing new voter to the end of json file
+                jsonIO.writeToEndOfFile(newVoter);
 
                 //Creating Linear Layout record for new voter
                 if (voters.size() > 0){
@@ -186,112 +183,4 @@ public class MainActivity extends AppCompatActivity {
         return dp * ((float) getResources().getDisplayMetrics().densityDpi / DisplayMetrics.DENSITY_DEFAULT);
     }
 
-    //Creating Object for JSON file
-    public JSONObject createJSONObject() {
-        JSONObject jsonFileObject = new JSONObject();
-        JSONArray jsonArray = new JSONArray();
-        try {
-            jsonFileObject.put("voters", jsonArray);
-        } catch (JSONException e) {
-            e.printStackTrace();
-        }
-        return jsonFileObject;
-    }
-
-    //Adding a voter to JSON array
-    private void putVoter(Voter voter, JSONArray jsonArray) {
-        JSONObject newVoter = new JSONObject();
-        try {
-            newVoter.put("timestamp", voter.getTime());
-            newVoter.put("count", voter.getCount());
-            newVoter.put("formattedDate", voter.getFormattedDate());
-            newVoter.put("formattedTime", voter.getFormattedTime());
-            newVoter.put("text", voter.getText());
-            jsonArray.put(newVoter);
-        } catch (JSONException e) {
-            e.printStackTrace();
-        }
-    }
-
-    //Adding a voter to JSON file Object
-    public JSONObject addVoterToJSON(JSONObject jsonObject, Voter voter, String arrayKey) {
-        try {
-            putVoter(voter, jsonObject.getJSONArray(arrayKey));
-        } catch (JSONException e) {
-            e.printStackTrace();
-        }
-        return jsonObject;
-    }
-
-    //Reading a whole JSON file
-    public String readJSONFromFile(String path) {
-        StringBuilder stringBuilder = new StringBuilder();
-        try {
-            File file = new File(this.getFilesDir(), path);
-            FileReader fileReader = new FileReader(file);
-            BufferedReader bufferedReader = new BufferedReader(fileReader);
-            String line = bufferedReader.readLine();
-            while (line != null) {
-                stringBuilder.append(line);
-                line = bufferedReader.readLine();
-            }
-            bufferedReader.close();
-            fileReader.close();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        return stringBuilder.toString();
-    }
-
-    //Writing a whole JSON Object to a file
-    public void writeJSONToFile(JSONObject jsonObject, String path) {
-        try {
-            File file = new File(this.getFilesDir(), path);
-            FileWriter fileWriter = new FileWriter(file);
-            BufferedWriter bufferedWriter = new BufferedWriter(fileWriter);
-            bufferedWriter.write(jsonObject.toString(1));
-            bufferedWriter.close();
-            fileWriter.close();
-        } catch (IOException | JSONException e) {
-            e.printStackTrace();
-        }
-    }
-
-    /*An attempt to make writing to the end of JSON
-    public void writeJSONToFile(JSONObject jsonObject, String path, boolean isAppend) {
-        try {
-            File file = new File(this.getFilesDir(), path);
-            FileWriter fileWriter = new FileWriter(file, isAppend);
-            BufferedWriter bufferedWriter = new BufferedWriter(fileWriter);
-            bufferedWriter.write(jsonObject.toString(1));
-            bufferedWriter.close();
-        } catch (IOException | JSONException e) {
-            e.printStackTrace();
-        }
-    }*/
-
-    //Parsing voters from JSON Object and adding them to list
-    private void parseJson(JSONObject response) {
-        try {
-            JSONArray stageArray = response.getJSONArray("voters");
-            voters = new ArrayList<Voter>();
-
-            for (int i = 0; i < stageArray.length(); i++) {
-                JSONObject jsonStage = (JSONObject) stageArray.get(i);
-
-                long timestamp = jsonStage.getLong("timestamp");
-                //Log.e("ReadTime", String.valueOf(timestamp));
-                int count = jsonStage.getInt("count");
-                //Log.e("ReadCount", String.valueOf(count));
-
-                Voter voter = new Voter(timestamp, count);
-
-                voters.add(voter);
-            }
-
-        } catch (JSONException e) {
-            e.getMessage();
-            e.printStackTrace();
-        }
-    }
 }
