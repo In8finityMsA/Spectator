d5fa9f3befae3a146500fd37a54219d062b01a21
Richard Ragusski <richard-on@mail.ru>
2020-07-19 14:15:34 +0300

    Start page added.

---
+++ app/src/main/java/com/spectator/Start.java
@@ -0,0 +1,35 @@
+package com.spectator;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.spectator.menu.Menu;
+
+    public class Start extends AppCompatActivity {
+
+        private TextView start;
+
+        protected void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.start);
+
+            start = (TextView) findViewById(R.id.start);
+
+            start.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    Intent intent = new Intent(getApplicationContext(), Menu.class);
+
+                    startActivity(intent);
+                }
+
+            });
+        }
+
+
+
+}
