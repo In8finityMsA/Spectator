16be633d39134801ab786af591d6bbfd6841098e
Richard Ragusski <richard-on@mail.ru>
2020-07-21 15:25:40 +0300

    New counter design.

---
+++ app/src/main/java/com/spectator/DetailsFragment.java
@@ -0,0 +1,23 @@
+package com.spectator;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+public class DetailsFragment {
+
+    public DetailsFragment() {
+    }
+
+    @Nullable
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.other_fragment, container, false);
+
+        return view;
+
+    }
+}
